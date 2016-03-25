3305e8344d326da1a364a911740a7224ed5dffe4
Adam Bray <adam.bray@yale.edu>
2016-03-25 09:21:31 -0400

    adds savings deposit functionality

--- atm.js
+++ atm.js
@@ -1,13 +1,25 @@
 // your code goes here!
 var checkingBalance = 0;
+var savingsBalance  = 0;
 var checkingDepositButton = document.querySelector("#checkingDepositButton");
+var savingsDepositButton  = document.querySelector("#savingsDepositButton");
 
 //event listener for deposit
 checkingDepositButton.addEventListener("click", depositIntoChecking);
+savingsDepositButton.addEventListener("click", depositIntoSavings);
 
 function depositIntoChecking(){
   var amount = document.querySelector("#checkingAmount").value;
   checkingBalance = checkingBalance + parseInt(amount);
+
   var balanceDiv = document.querySelector("#checkingBalanceDiv");
   balanceDiv.textContent = "$" + checkingBalance;
+}
+
+function depositIntoSavings(){
+  var amount = document.querySelector("#savingsAmount").value;
+  savingsBalance = savingsBalance + parseInt(amount);
+
+  var balanceDiv = document.querySelector("#savingsBalanceDiv");
+  balanceDiv.textContent = "$" + savingsBalance;
 }
