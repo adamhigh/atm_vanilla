36958dae2bddb54cba2e25218f82a920e13053ed
Adam Bray <adam.bray@yale.edu>
2016-03-25 09:16:13 -0400

    adds deposit functionality

--- atm.js
+++ atm.js
@@ -1,1 +1,13 @@
 // your code goes here!
+var checkingBalance = 0;
+var checkingDepositButton = document.querySelector("#checkingDepositButton");
+
+//event listener for deposit
+checkingDepositButton.addEventListener("click", depositIntoChecking);
+
+function depositIntoChecking(){
+  var amount = document.querySelector("#checkingAmount").value;
+  checkingBalance = checkingBalance + parseInt(amount);
+  var balanceDiv = document.querySelector("#checkingBalanceDiv");
+  balanceDiv.textContent = "$" + checkingBalance;
+}
